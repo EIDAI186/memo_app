9fd6b5a6464954922980fb10d50d493c598ca8f4
EIDAI186 <8eidai.jp@gmail.com>
2025-06-01 23:57:13 +0900

    new

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            NotepadTextBox = new TextBox();
+            NotepadMenuStrip = new MenuStrip();
+            ファイルToolStripMenuItem = new ToolStripMenuItem();
+            NewToolStripMenuItem = new ToolStripMenuItem();
+            ReadToolStripMenuItem = new ToolStripMenuItem();
+            SaveToolStripMenuItem = new ToolStripMenuItem();
+            NotepadSaveFileDialog = new SaveFileDialog();
+            NotepadOpenFileDialog = new OpenFileDialog();
+            NotepadMenuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // NotepadTextBox
+            // 
+            NotepadTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            NotepadTextBox.Location = new Point(1, 31);
+            NotepadTextBox.Multiline = true;
+            NotepadTextBox.Name = "NotepadTextBox";
+            NotepadTextBox.ScrollBars = ScrollBars.Both;
+            NotepadTextBox.Size = new Size(800, 421);
+            NotepadTextBox.TabIndex = 0;
+            NotepadTextBox.WordWrap = false;
+            NotepadTextBox.ModifiedChanged += NotepadTextBox_ModifiedChanged;
+            // 
+            // NotepadMenuStrip
+            // 
+            NotepadMenuStrip.ImageScalingSize = new Size(20, 20);
+            NotepadMenuStrip.Items.AddRange(new ToolStripItem[] { ファイルToolStripMenuItem });
+            NotepadMenuStrip.Location = new Point(0, 0);
+            NotepadMenuStrip.Name = "NotepadMenuStrip";
+            NotepadMenuStrip.Size = new Size(800, 28);
+            NotepadMenuStrip.TabIndex = 1;
+            NotepadMenuStrip.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            ファイルToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { NewToolStripMenuItem, ReadToolStripMenuItem, SaveToolStripMenuItem });
+            ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            ファイルToolStripMenuItem.Size = new Size(65, 24);
+            ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // NewToolStripMenuItem
+            // 
+            NewToolStripMenuItem.Name = "NewToolStripMenuItem";
+            NewToolStripMenuItem.Size = new Size(152, 26);
+            NewToolStripMenuItem.Text = "新規作成";
+            NewToolStripMenuItem.Click += NewToolStripMenuItem_Click;
+            // 
+            // ReadToolStripMenuItem
+            // 
+            ReadToolStripMenuItem.Name = "ReadToolStripMenuItem";
+            ReadToolStripMenuItem.Size = new Size(152, 26);
+            ReadToolStripMenuItem.Text = "読み込み";
+            ReadToolStripMenuItem.Click += ReadToolStripMenuItem_Click;
+            // 
+            // SaveToolStripMenuItem
+            // 
+            SaveToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            SaveToolStripMenuItem.Size = new Size(152, 26);
+            SaveToolStripMenuItem.Text = "保存";
+            SaveToolStripMenuItem.Click += SaveToolStripMenuItem_Click;
+            // 
+            // NotepadSaveFileDialog
+            // 
+            NotepadSaveFileDialog.Filter = "テキストファイル|*.txt|すべてのファイル|*.*";
+            NotepadSaveFileDialog.FileOk += NotepadSaveFileDialog_FileOk;
+            // 
+            // NotepadOpenFileDialog
+            // 
+            NotepadOpenFileDialog.FileName = "openFileDialog1";
+            NotepadOpenFileDialog.Filter = "テキストファイル|*.txt|すべてのファイル|*.*";
+            NotepadOpenFileDialog.FileOk += NotepadOpenFileDialog_FileOk;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(NotepadTextBox);
+            Controls.Add(NotepadMenuStrip);
+            MainMenuStrip = NotepadMenuStrip;
+            Name = "Form1";
+            Text = "無題-Notepad";
+            FormClosing += Form1_FormClosing;
+            NotepadMenuStrip.ResumeLayout(false);
+            NotepadMenuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private MenuStrip NotepadMenuStrip;
+        private ToolStripMenuItem ファイルToolStripMenuItem;
+        private ToolStripMenuItem NewToolStripMenuItem;
+        private ToolStripMenuItem ReadToolStripMenuItem;
+        private ToolStripMenuItem SaveToolStripMenuItem;
+        private SaveFileDialog NotepadSaveFileDialog;
+        private OpenFileDialog NotepadOpenFileDialog;
+        protected TextBox NotepadTextBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+namespace Notepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool SaveConfirmation()
+        {
+            if (NotepadTextBox.Modified == true)
+            {
+                DialogResult result = MessageBox.Show("内容が変更されています。保存しますか？",
+                    "ファイルの保存確認", MessageBoxButtons.YesNoCancel);
+                if (result == DialogResult.Yes)
+                {
+                    // ファイルに保存
+                    result = NotepadSaveFileDialog.ShowDialog();
+                    if (result == DialogResult.OK)
+                    {
+                        return true;
+                    }
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 新規作成ボタンの選択
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool result = SaveConfirmation();
+            if (result == true)
+            {
+                NotepadTextBox.Text = "";
+                Text = "無題 - Notepad";
+                NotepadTextBox.Modified = false;
+            }
+        }
+
+        /// <summary>
+        /// 読み込みボタンの選択
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool result = SaveConfirmation();
+            if (result == true)
+            {
+                NotepadOpenFileDialog.ShowDialog();
+            }
+        }
+
+        /// <summary>
+        /// 保存ボタンの選択
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NotepadSaveFileDialog.ShowDialog();
+            //StreamWriter sw = new StreamWriter("text.txt", false);
+            //sw.Write(NotepadTextBox.Text);
+            //sw.Close();
+        }
+
+        /// <summary>
+        /// ファイルの保存、OKボタン押下
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NotepadSaveFileDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(NotepadSaveFileDialog.FileName, false);
+            sw.Write(NotepadTextBox.Text);
+            sw.Close();
+
+            string name = Path.GetFileName(NotepadSaveFileDialog.FileName);
+            Text = name + " - Notepad";
+
+            NotepadTextBox.Modified = false;
+        }
+
+        /// <summary>
+        /// ファイルを開く、OKボタン押下
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NotepadOpenFileDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StreamReader sr = new StreamReader(NotepadOpenFileDialog.FileName, false);
+            NotepadTextBox.Text = sr.ReadToEnd();
+            sr.Close();
+
+            string name = Path.GetFileName(NotepadOpenFileDialog.FileName);
+            Text = name + " - Notepad";
+
+            NotepadTextBox.Modified = false;
+        }
+
+        private void NotepadTextBox_ModifiedChanged(object sender, EventArgs e)
+        {
+            if (NotepadTextBox.Modified == true)
+            {
+                Text = "*" + Text;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bool result = SaveConfirmation();
+            if (result == false) { 
+               e.Cancel = true;
+            }
+        }
+    }
+}
